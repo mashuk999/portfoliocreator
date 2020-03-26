dd20e0cd517d4e877e00441f87f1acf7bd797595
mashuk999 <mashuk1998@gmail.com>
2020-03-26 21:15:38 +0530

    django-heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import cloudinary
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -142,3 +143,6 @@ cloudinary.config(
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
